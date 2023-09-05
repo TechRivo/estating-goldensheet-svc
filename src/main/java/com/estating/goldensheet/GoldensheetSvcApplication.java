64c33407c85c7d8ec4f757a304656a06db1e9d66
patrykmajchrzycki <patryk.majchrzycki@gmail.com>
2023-09-05 14:13:31 +0200

    #862kb445k Parse Goldensheet

---
+++ src/main/java/com/estating/goldensheet/GoldensheetSvcApplication.java
@@ -0,0 +1,13 @@
+package com.estating.goldensheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldensheetSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldensheetSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estating/goldensheet/GoldensheetSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estating.goldensheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldensheetSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
